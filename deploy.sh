8368268535d229e3f3aa5fcf3daec34a91474599
balazsszilardtoth <balazs.szilard.toth@icloud.com>
2019-04-23 17:31:16 +0200

    added deployments scripts

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+docker build -t allotsys/multi-client:latest -t allotsys/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t allotsys/multi-server:latest -t allotsys/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t allotsys/multi-worker:latest -t allotsys/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push allotsys/multi-client:latest
+docker push allotsys/multi-server:latest
+docker push allotsys/multi-worker:latest
+docker push allotsys/multi-client:$SHA
+docker push allotsys/multi-server:$SHA
+docker push allotsys/multi-worker:$SHA
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=allmyles/multi-server:$SHA
+kubectl set image deployments/client-deployment client=allmyles/multi-client:$SHA
+kubectl set image deplomyents/worker-deployment worker=allmyles/multi-worker:$SHA
